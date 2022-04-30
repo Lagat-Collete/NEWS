c224fe86fdf728ee47c3dd04b69f36fa60c47f0c
colletemine <jepngetichcolleta@gmail.com>
2022-04-30 22:17:31 +0300

    add test and objects

--- app/views.py
+++ app/views.py
@@ -1,4 +1,15 @@
 from flask import render_template
 from app import app
+from .request import get_sources,get_articles,get_topHeadLines
+
 
 #views
+@app.route('/')
+def index():
+  '''
+  View root page function that returns the index page and its data
+  '''
+
+  news_sources = get_sources()
+  articles = get_articles()
+  topHeadLines = get_topHeadLines
